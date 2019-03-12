722d3fb99bbf02df99b039f975f85a069da8b56d
Eleks <msdn@eleks.com>
2019-03-12 10:46:25 +0200

    1

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -10,214 +10,57 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {           
+        {
 
-            OperationsWithCollections gg = new OperationsWithCollections();
-            gg.GetMean(gg.numbers);
-            gg.GetMedian(gg.numbers);
-            gg.GetMode(gg.numbers);
-            gg.FindElementsInList(gg.ShuffleList(gg.strings), "Peter");
-            gg.ParseInt(gg.strList);          
-
+            Book gg = new Book("Glory", 23);
+            gg.Info();
+            MyBook jj = new MyBook("Some book", 23, "John");
+            jj.Info();
 
             Console.ReadKey();
         }
     }
 
+    abstract class Item
+    {       
 
+        public abstract void Info(); 
+    }
 
-    class OperationsWithCollections
+    class Book : Item
     {
-        public List<int> numbers = new List<int>() { 1, 2, 4, 2, 3, 45 };
-        public List<string> strings = new List<string>() { "Ivan", "Peter", "Adam", "John", "Adam", "Janes", "Janes" };
-        public List<string> strList = new List<string>() { "Ann", "34", "Dog", "Aplle 64" };
+        protected string name { get; set;}
+        protected int price { get; set; }
 
-
-        public double GetMean(List<int> list)
+        public Book(string name, int price)
         {
-            double mean = 0;
-            int sum = 0;
-            int i = 0;
-
-            do
-            {
-                sum += list[i];
-                i++;
-            } while (i < list.Count());
-
-            // for  (int i = 0; i < list.Count(); i++)
-            // {
-            //     sum += list[i];
-            //}
-
-            mean = sum / list.Count();
-            Console.WriteLine("Mean - " + mean);
-
-            return mean;
+            this.name = name;
+            this.price = price;
         }
 
-        public double GetMedian(List<int> list)
+        override public void Info()
         {
-            double median = 0;
-            list.Sort();
-
-            if (list.Count() % 2 == 0)
-            {
-
-                median = (list[list.Count() / 2] + list[(list.Count() / 2) + 1]) / 2;
-
-            }
-            else
-            {
-                median = list[(int)Math.Round((double)list.Count() / 2)];
-            }
-
-            Console.WriteLine("Median - " + median);
-
-            return median;
+            Console.WriteLine("Book name - {0}, Price - {1}", name, price);
+        }
+    }
+    class MyBook : Book
+    {
+        protected string owner { get; set; }
+        public MyBook(string name, int price, string owner) : base(name, price)
+        {
+            this.name = name;
+            this.price = price;
+            this.owner = owner;
         }
 
-        public int GetMode(List<int> list)
+        override public void Info()
         {
-
-            int mode = default(int);
-
-            if (list != null && list.Count() > 0)
-            {
-
-                Dictionary<int, int> counts = new Dictionary<int, int>();
-
-                foreach (int element in list)
-                {
-                    if (counts.ContainsKey(element))
-                        counts[element]++;
-                    else
-                        counts.Add(element, 1);
-                }
-
-                int max = 0;
-                foreach (KeyValuePair<int, int> count in counts)
-                {
-                    if (count.Value > max)
-                    {
-
-                        mode = count.Key;
-                        max = count.Value;
-                    }
-                }
-            }
-
-            Console.WriteLine("Mode - " + mode);
-            return mode;
-        }
-
-        public void FindElementsInList(List<string> list, string str)
-
-
-        {
-
-
-            int i = 0;
-
-            foreach (string el in list)
-            {
-
-                if (el == str)
-                {
-                    if (el == "Janes")
-                    {
-                        i++;
-                        Console.WriteLine(" James Found");
-                        break;
-                    }
-                    if (el == "Adam")
-                    {
-                        i++;
-                        Console.WriteLine("Adam Found");
-                        continue;
-                        Console.WriteLine("11");
-                    }
-                    else
-                    {
-                        i++;
-                    }
-                }
-            }
-
-            if (i > 0)
-            {
-                Console.WriteLine("Succes , you found - {0} in array {1} times ", str, i);
-            }
-            else
-            {
-                Console.WriteLine("Fail  , you did not  find - {0} in array  ", str);
-            }
-
-        }
-
-        public List<string> ShuffleList(List<string> list)
-        {
-            Random random = new Random();
-
-            for (int i = 0; i < list.Count; i++)
-            {
-                string temp = list[i];
-                int randomIndex = random.Next(i, list.Count);
-                list[i] = list[randomIndex];
-                list[randomIndex] = temp;
-
-                //Console.WriteLine(list[randomIndex]);
-
-            }
-
-            return list;
-        }
-
-        public void ParseInt(List<string> list)
-        {
-            int[] ints = new int[list.Count];
-            int i = 0;
-
-            foreach (string s in list)
-            {
-
-                int parsedInt = 0;
-                if (int.TryParse(s, out parsedInt))
-                {
-                    ints[i] = parsedInt;
-                    Console.WriteLine(ints[i]);
-
-                    i++;
-                }
-                else
-                {
-                    string b = string.Empty;
-
-                    for (int k = 0; k < s.Length; k++)
-                    {
-                        if (Char.IsDigit(s[k]))
-                            b += s[k];
-                    }
-
-
-                    if (b.Length > 0)
-                    {
-                        ints[i] = int.Parse(b);
-                        Console.WriteLine(int.Parse(b));
-
-                    }
-
-                    i++;
-
-                }
-            }
-            foreach (int elm in ints)
-            {
-                Console.WriteLine("q -" + elm);
-            }
+            Console.WriteLine("Book name - {0}, Price - {1}, Owner - {2}", name, price,owner);
         }
     }
 }
+
+
     
 
 
